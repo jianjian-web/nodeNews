0f5d27e9dc7a4f4a2889831c2b79cc749e3f98f3
zhoujian <758507292@qq.com>
2017-12-01 10:59:49 +0800

    增加登录注册

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const app = new express();
 const mongo = require('./model/db.js');
 const formidable = require('formidable');
 const form = new formidable.IncomingForm();
+const session = require("express-session");
 
 app.set('view engine','ejs');
 app.use(express.static('./public'));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+
 
 app.get('/',(req,res)=>{
   mongo._connect().then((db)=>{
@@ -33,6 +41,65 @@ app.post('/add',(req,res)=>{
           });
         });
   })
+});
+
+app.get('/login', (req,res)=>{
+  res.render('login',{});
+});
+app.post('/login',(req,res)=>{
+  form.parse(req, function(err, fields, files, next) {
+    if(err){
+      next();
+      return;
+    }
+  mongo._connect().then((db)=>{
+    console.dir(fields.uname)
+    mongo.find('news',{uname:fields.uname}).then(result =>{
+      if(result.length){
+        res.send({"message":"用户名已注册","status":-1});
+        return
+      }
+      mongo.insertOne('news',fields).then(resultValue=>{
+        res.send({"message":"注册成功,请返回登录","status":1});
+      });
+    });
+  });
+});
+});
+
+app.get('/sign', (req,res)=>{
+  res.render('sign',{});
+});
+app.post('/sign',(req,res)=>{
+  form.parse(req, function(err, fields, files, next) {
+    if(err){
+      next();
+      return;
+    }
+    mongo.find('news',{uname:fields.uname}).then(result =>{
+      console.dir(result[0].upwd)
+      if(result.length){
+        if(result[0].upwd == fields.upwd){
+          req.session.login = "success";
+          req.session.username = fields.uname;
+          res.send({"message":"登录成功","status":2})
+        }
+      }else{
+
+      }
+    });
+});
+});
+app.use('*',(req,res,next)=>{
+  console.log(req.originUrl)
+  if(req.session.login==='success'){
+    next()
+  }else{
+    res.status(401).json({"message":"请登录"});
+  }
+})
+app.get('/test',(req,res)=>{
+  res.send('这是test');
 });
 
 app.all('*',(req,res)=>{
